698beeb7b0e6d394d7bee8c96444593c182d3712
Jacky Tang <jackytck@gmail.com>
2019-10-04 13:52:32 +0800

    :memo: descriptions

--- cmd/root.go
+++ cmd/root.go
@@ -15,16 +15,8 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "jcconv",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Traditional/simplified chinese converter.",
+	Long:  "Translate between HK, TW traditional and simplified chinese, with auto detection and api server.",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
