6970763c7a5d4071ddad4711ee443a3d63bf2371
Jacky Tang <jackytck@gmail.com>
2019-10-04 13:56:03 +0800

    :sparkles: bash completion

---
+++ cmd/completion.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/jackytck/alti-cli/errors"
+	"github.com/spf13/cobra"
+)
+
+// completionCmd represents the completion command
+var completionCmd = &cobra.Command{
+	Use:   "completion",
+	Short: "Generates bash completion scripts",
+	Long: `To load completion run:
+
+[Linux]:
+  sudo yum install bash-completion -y
+  echo "source <(jcconv completion)" >> ~/.bashrc
+[Mac]:
+  brew install bash-completion@2
+  jcconv completion > $(brew --prefix)/etc/bash_completion.d/jcconv
+
+Then restart shell.
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		errors.Must(rootCmd.GenBashCompletion(os.Stdout))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(completionCmd)
+}
