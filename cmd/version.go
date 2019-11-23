1e44d7f242151c28026b8cf7891375c916622b32
Jacky Tang <jackytck@gmail.com>
2019-11-23 14:21:09 +0800

    :wrench: homebrew config

---
+++ cmd/version.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Version info.",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("v1.0.2")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
